f3aaf8cc29c648316438d09733cd5758a9eced31
Anthony <awrodriguezt@gmail.com>
2020-04-12 02:23:17 -0600

    Create Course

--- lbfrontend/src/Components/Common/Validators/Validators.js
+++ lbfrontend/src/Components/Common/Validators/Validators.js
@@ -1,5 +1,6 @@
 export const emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
 export const emptyRegex = /^\s?$/;
 export const passwordRegex = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{7,19})\S$/;
-export const nameRegex = /^\s?[a-zA-Z]{2,}([- ][a-zA-Z]{2,}){1,3}\s?$/;
+export const nameRegex = /^\s?[a-zA-Záóíúé]{2,}([- ][a-zA-Záóíúé]{2,}){1,3}\s?$/;
 export const edadRegex = /^\d{1,3}$/
+export const longStringRegex = /^[a-zA-Záóíúé]{4,}(\s?[a-zA-Záóíúé]{1,}){0,}$/;
